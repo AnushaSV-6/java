b4bc08a36204a4b0b5136429e8231876cc532d4f
Anusha <anushasv02@gmail.com>
2026-06-11 16:04:29 +0530

    Create BalancedParentheses.java

---
+++ BalancedParentheses.java
@@ -0,0 +1,30 @@
+//Given a string containing brackets (), {}, [], check if it is balanced.
+import java.util.*;
+
+public class BalancedParentheses {
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') return false;
+                if (ch == '}' && top != '{') return false;
+                if (ch == ']' && top != '[') return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String str = "{[()]}";
+
+        System.out.println(isBalanced(str));
+    }
+}
